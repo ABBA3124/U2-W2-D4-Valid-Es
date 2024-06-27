8bd722a33bd4d85dbedef692d664a6e96ee2fa3c
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-27 14:38:48 +0200

    Initial commit

---
+++ src/main/java/davideabbadessa/U2_W2_D4_Valid_Es/U2W2D4ValidEsApplication.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W2_D4_Valid_Es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W2D4ValidEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W2D4ValidEsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davideabbadessa/U2_W2_D4_Valid_Es/U2W2D4ValidEsApplicationTests.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W2_D4_Valid_Es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D4ValidEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
